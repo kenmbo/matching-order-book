2fa3d51fbb940a1fdc8960cdf4401c70abeb688b
Kenneth Bolando <kenneth.bolando@siu.edu>
2026-08-06 20:40:59 -0500

    feat: add C++ starting code

---
+++ include/lob/project.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string_view>
+
+namespace lob {
+
+[[nodiscard]] std::string_view project_name() noexcept;
+
+}  // namespace lob
---
+++ src/project.cpp
@@ -0,0 +1,7 @@
+#include "lob/project.hpp"
+
+namespace lob {
+
+std::string_view project_name() noexcept { return "limit_order_book"; }
+
+}  // namespace lob
